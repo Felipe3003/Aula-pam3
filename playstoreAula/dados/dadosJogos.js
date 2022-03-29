ef8758e36cc0eb84d80cbff4f58d8fb60622ca82
Felipe3003 <you@example.com>
2022-03-29 10:48:02 -0300

    iuuy

---
+++ playstoreAula/dados/dadosJogos.js
@@ -0,0 +1,35 @@
+const JOGOS = [
+    {
+        id : "01",
+        nome : "Subway Surfers";
+        valor : 13.32,
+        imagem: 'subway.jpg'
+    },
+    {
+        id: "02",
+        nome: "yugioh Master Duel",
+        valor: "gratuito",
+        imagem: "yugi.jpg",
+    },
+    {
+        id : "03"
+        nome: "free fire",
+        valor: 50.00,
+        imagem : "frifas.jpg",
+    },
+    {
+        id: "04",
+        nome : "fortnite",
+        valor: 350.00,
+        imagem: "fortnite.jpg"
+    },
+    {
+        id: "05"
+        nome: "jogo do foguinho",
+        valor: 15.00,
+        imagem: "tinder.png",
+    },
+    {  id: "06"
+        nome: "tok tik",
+        valor: "codigo do kwai",
+        imagem: "tiktok.png"
